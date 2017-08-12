8503a37806fb729265c786cd703aca9a200b5b3c
SHewitt95 <s.hewitt7@umiami.edu>
2017-08-12 19:07:57 -0400

    delete_messages works now lol

--- delete_messages.py
+++ delete_messages.py
@@ -4,21 +4,27 @@ from twilio.rest import Client
 
 account_sid = os.environ["TWILIO_ACCOUNT_SID"]
 auth_token = os.environ["TWILIO_AUTH_TOKEN"]
-client = TwilioRestClient(account_sid, auth_token)
+client = Client(account_sid, auth_token)
 
-delete_messages(number, on_after_date=None)
-    if on_after_date is not None
+def delete_messages(number, on_after_date=None):
+    """
+    Deletes all "Incoming" messages from the Twilio logs.
+    """
+    if on_after_date is not None:
         messages = client.messages.list(to=number, date_sent=on_after_date)
+    else:
+        return
 
     counter = 0
     for message in messages:
-        client.messages.delete(message.sid)
+        client.messages(message.sid).delete()
         counter += 1
 
-    print("Deleted {} messages".format(counter))
+    print("Deleted {} message(s)".format(counter))
 
 def main():
-    delete_messages(os.environ['TWILIO_NUMBER'])
+    today = date.today()
+    delete_messages(os.environ['TWILIO_NUMBER'], today)
 
 if __name__=='__main__':
     main()
