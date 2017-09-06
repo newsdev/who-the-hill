6f6a5e72681a1821f132a7b34d63ab7b7e282d73
SHewitt95 <s.hewitt7@umiami.edu>
2017-09-05 22:49:13 -0400

    Test face.py

--- tests/face_test.py
+++ tests/face_test.py
@@ -1,7 +1,22 @@
 import unittest
-import face
+from face import Face
+
+class FaceTest(unittest.TestCase):
+    def setUp(self):
+        dummy_data = {"left": 100, "top": 110, "width": 120, "height": 130, "confidence": 99, "name": "Dummy"}
+
+        self.my_face_dummy = Face(dummy_data["left"], dummy_data["top"], dummy_data["width"], dummy_data["height"], dummy_data["confidence"], dummy_data["name"])
+
+        self.my_face = Face()
+
+    def test_my_face_dummy_confidence(self):
+        self.assertTrue(self.my_face_dummy.to_dict()["MatchConfidence"] > -1)
+
+    def test_my_face_dummy_bounding_box(self):
+        for k,v in self.my_face_dummy.to_dict()["BoundingBox"].items():
+            with self.subTest(k=k, v=v):
+                self.assertTrue(v > -1)
 
 
-
-if __name__ == "__main__":
-    pass
+if __name__ == '__main__':
+    unittest.main()
