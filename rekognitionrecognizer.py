c74e37c111c134d4b8cf8814e31b16dd12a9dafe
Jeremy Bowers <jeremyjbowers@gmail.com>
2017-09-18 16:14:15 -0400

    Error handling in the recognizer

--- rekognitionrecognizer.py
+++ rekognitionrecognizer.py
@@ -1,16 +1,28 @@
 import json
+import logging
 import requests
 
 from image import Image
 from abstractrecognizer import AbstractRecognizer
 
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.DEBUG)
+log.addFilter(healthcheck_filter)
+logging.basicConfig(level=logging.INFO)
+
 class RekognitionRecognizer(AbstractRecognizer):
     """ An implementation of the AbstractRecognizer class to work with our Rekognition wrapper server """
 
     def recognize(self, image):
+        logging.info("Getting image file")
         image.get_image_file().seek(0)
+
+        logging.info("Posting to rekognition at %s" % self.recognition_endpoint)
         r = requests.post(self.recognition_endpoint, files={'file': image.get_image_file().read()})
+
+        logging.info("Face object returned: %s" % r.text)
         face_obj = json.loads(r.text)
+
         if ("CelebrityFaces" in face_obj and len(face_obj['CelebrityFaces']) > 0):
             celeb_objs = face_obj['CelebrityFaces']
             for obj in celeb_objs:
