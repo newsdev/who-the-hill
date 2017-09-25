126e4bfd33353b337a914ffaef3d28be69b9c614
Jeremy Bowers <jeremyjbowers@gmail.com>
2017-09-25 16:48:54 -0400

    PLEASE

--- twilio_app.py
+++ twilio_app.py
@@ -270,7 +270,7 @@ def persist_file(filename, uploaded_file):
     persists a file to google cloud.
     '''
     client = storage.Client()
-    bucket = client.get_bucket('nytint-stg-newsapps')
+    bucket = client.get_bucket('int.nyt.com')
     local_filename = path.split('/')[-1]
     remote_path = '%s/%s' % ('shazongress', local_filename)
     blob = bucket.blob(remote_path)
